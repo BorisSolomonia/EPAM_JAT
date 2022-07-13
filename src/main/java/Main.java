cc1b39c150bfd1d70291837b2cdfccfd2a5b413a
Boris Solomonia <borissolomoniaphone@gmail.com>
2022-07-13 23:20:13 +0400

    first commit of the nested

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Customer1 officeProfile = new Customer1(){
             public void printCustomer(){
-                System.out.println("customer was in office");
+                System.out.println("customer was in office today");
             }
         };
     }
